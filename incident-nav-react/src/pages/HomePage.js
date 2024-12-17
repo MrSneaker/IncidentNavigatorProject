88a4f65f6f88a83f5e6cee9508d872f83c8e3f02
axel <axel.colmant@etu.univ-lyon1.fr>
2024-12-17 22:48:06 +0100

    #2 Finissions page home

--- incident-nav-react/src/pages/HomePage.js
+++ incident-nav-react/src/pages/HomePage.js
@@ -4,22 +4,25 @@ import { Link } from "react-router-dom";
 export default function HomePage({ isAuthenticated }) { 
     return (
       <div className="flex flex-col items-center justify-center h-full w-full"> 
-        <h1 className="text-4xl font-bold dark:text-dark-accent text-light-text">Welcome to Incident Navigator</h1>
-        <p className="text-center text-base mt-4 max-w-xl">
+        <h1 className="text-center font-bold dark:text-dark-accent text-light-text"
+            style={{ fontSize: 'clamp(30px, 5vw, 60px)' }}>
+            Welcome to Incident Navigator
+        </h1>
+        <p className="text-center text-base mt-4 max-w-2xl">
             Discover smarter, faster incident resolution with IncidentNavigator. Our system combines Large Language Models (LLMs) and Retrieval-Augmented Generation (RAG) to offer dynamic, interactive, and traceable incident support.
         </p>        
 
         {!isAuthenticated ? (
             <div className="flex flex-row mt-6">
-                <Link to='/login' className="btn btn-primary m-2 rounded-full p-2 min-w-[100px] text-center font-bold border-2 border-dark-accent text-dark-accent hover:text-dark-background hover:bg-dark-accent hover:scale-110 transition-transform">
+                <Link to='/login' className="btn btn-primary m-2 rounded-full p-2 min-w-[100px] max-w-[200px] text-center font-bold border-2 border-dark-accent text-dark-accent hover:text-dark-background hover:bg-dark-accent hover:scale-110 transition-transform">
                     Login
                 </Link>
-                <Link to='/register' className="btn btn-primary m-2 rounded-full p-2 min-w-[100px] text-center font-bold border-2 border-dark-accent text-dark-accent hover:text-dark-background hover:bg-dark-accent hover:scale-110 transition-transform">
+                <Link to='/register' className="btn btn-primary m-2 rounded-full p-2 min-w-[100px] max-w-[200px] text-center font-bold border-2 border-dark-accent text-dark-accent hover:text-dark-background hover:bg-dark-accent hover:scale-110 transition-transform">
                     Register
                 </Link>
             </div>
         ) : (
-            <Link to="/chat" className="btn btn-secondary m-2 rounded-full bg-gradient-to-r text-center from-dark-accent to-light-accent p-2 min-w-[100px] hover:scale-105 transition-transform flex px-4 py-2 items-center">
+            <Link to="/chat" className="btn btn-secondary m-2 rounded-full bg-gradient-to-r text-center from-dark-accent to-light-accent p-2 min-w-[100px] max-w-[200px] hover:scale-105 transition-transform flex px-4 py-2 items-center">
                 Start Chatting <GoArrowUpRight className="text-2xl ml-2" />
             </Link>
         )}
